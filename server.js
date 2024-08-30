f004e89bad5344486cf2ce312059d92c4f7e1d8e
sirachuchTT31 <sirachuch.kho@softpomarket.com>
2024-08-30 09:58:23 +0700

    fix : server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
+app.get("/api/auth", (req, res) => {
     res.json({ message: "Server Running." });
 });
 
